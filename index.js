8e58a7cb7b492c3a117e2d9c1724b9dd750baa47
Ricardo Graça <ricardo@devius.net>
2014-08-22 12:30:16 +0100

    Add color to message type labels

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 if (console._isPatched) return;
 
+function addColor(string, name) {
+  var colors = {
+    green: ['\x1B[32m', '\x1B[39m'],
+    red: ['\x1B[1m\x1B[31m', '\x1B[39m\x1B[22m'],
+    yellow: ['\x1B[33m', '\x1B[39m']
+  }
+
+  return colors[name][0] + string + colors[name][1];
+}
+
+function getColorName(methodName) {
+  switch (methodName) {
+    case 'error': return 'red';
+    case 'warn': return 'yellow';
+    default: return 'green';
+  }
+}
+
 ['log', 'info', 'warn', 'error', 'dir', 'assert'].forEach(function(method) {
   var baseConsoleMethod = console[method];
   var slice = Array.prototype.slice;
+  var color = getColorName(method);
+  var output = method === 'warn' || method === 'error' ? 'stderr' : 'stdout';
 
   console[method] = function() {
     var date = (new Date()).toISOString();
     var args = slice.call(arguments);
-    var output = method === 'warn' || method === 'error' ? 'stderr' : 'stdout';
 
-    process[output].write('[' + date + '] ' + method.toUpperCase() + ' ');
+    process[output].write('[' + date + '] ' + addColor(method.toUpperCase(), color) + ' ');
 
     return baseConsoleMethod.apply(console, args);
   }
