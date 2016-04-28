f5f2f72cf8c439b1617b12a70944af95fad448db
Ricardo Graça <ricardo@devius.net>
2016-04-28 20:57:18 +0100

    Add cache module to cache console messages

---
+++ cache.js
@@ -0,0 +1,28 @@
+var util = require('util')
+var Cache = module.exports = function Cache(consoleMethod, name) {
+  this.cache = {
+    bufferedMessages: [],
+    size: 0
+  }
+  this.consoleMethod = consoleMethod
+  this.name = name
+
+  process.on('exit', this.flush.bind(this))
+}
+
+Cache.prototype.store = function(messageArguments) {
+  var formattedMessage = util.format.apply(console, messageArguments)
+
+  this.cache.size += Buffer.byteLength(formattedMessage)
+  this.cache.bufferedMessages.push(formattedMessage)
+
+  if (this.cache.size > 8192) this.flush()
+}
+
+Cache.prototype.flush = function(code) {
+  if (this.cache.size === 0) return
+
+  this.consoleMethod(this.cache.bufferedMessages.join('\n'))
+  this.cache.bufferedMessages.length = 0
+  this.cache.size = 0
+}
