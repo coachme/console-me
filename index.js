f7e012a4660a1b7b229954751deb859affbd4f2f
Ricardo Graça <ricardo@devius.net>
2016-04-28 18:07:56 +0100

    Remove all unnecessary semicolons

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-var isPatched = false;
-var slice = Array.prototype.slice;
+var isPatched = false
+var slice = Array.prototype.slice
 
-if (isPatched) return;
+if (isPatched) return
 
 function addColor(string) {
-  var colorName = getColorName(string);
+  var colorName = getColorName(string)
   var colors = {
     green: ['\x1B[32m', '\x1B[39m'],
     red: ['\x1B[1m\x1B[31m', '\x1B[39m\x1B[22m'],
     yellow: ['\x1B[33m', '\x1B[39m']
   }
 
-  return colors[colorName][0] + string + colors[colorName][1];
+  return colors[colorName][0] + string + colors[colorName][1]
 }
 
 function getColorName(methodName) {
   switch (methodName) {
-    case 'ERROR': return 'red';
-    case 'WARN': return 'yellow';
-    default: return 'green';
+    case 'ERROR': return 'red'
+    case 'WARN': return 'yellow'
+    default: return 'green'
   }
 }
 
 ['log', 'info', 'warn', 'error', 'dir', 'assert'].forEach(function(method) {
-  var baseConsoleMethod = console[method];
-  var messageType = method.toUpperCase();
-  var output = method === 'warn' || method === 'error' ? 'stderr' : 'stdout';
+  var baseConsoleMethod = console[method]
+  var messageType = method.toUpperCase()
+  var output = method === 'warn' || method === 'error' ? 'stderr' : 'stdout'
 
-  if (process[output].isTTY) messageType = addColor(messageType);
+  if (process[output].isTTY) messageType = addColor(messageType)
 
   console[method] = function() {
-    var date = (new Date()).toISOString();
-    var args = slice.call(arguments);
-    var dateMessage = '[' + date + '] ' + messageType + ' ';
+    var date = (new Date()).toISOString()
+    var args = slice.call(arguments)
+    var dateMessage = '[' + date + '] ' + messageType + ' '
 
-    args[0] = typeof args[0] === 'string' ? dateMessage + args[0] : dateMessage;
+    args[0] = typeof args[0] === 'string' ? dateMessage + args[0] : dateMessage
 
-    return baseConsoleMethod.apply(console, args);
+    return baseConsoleMethod.apply(console, args)
   }
-});
+})
 
-isPatched = true;
+isPatched = true
